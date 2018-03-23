9f759fff563491902be1eb0b69ce163dcbc46258
Igor Petelsky <igor.petelsky@gmail.com>
2018-03-23 15:50:32 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,33 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double balance;
+        int rate = 0;
+        BigDecimal total;
+
+        System.out.println("Please enter your balance ->");
+        Scanner inputNumScanner = new Scanner(System.in);
+        balance = (inputNumScanner.nextDouble());
+
+        if (balance < 1) {
+            rate = 0;
+        } else if (balance >= 1 & balance <= 100) {
+            rate = 3;
+        } else if (balance >= 100.01 & balance <= 1000) {
+            rate = 5;
+        } else if (balance >= 1000.01) {
+            rate = 7;
+        }
+
+        total = BigDecimal.valueOf((balance / 100 * rate + balance)).setScale(2, RoundingMode.DOWN);
+
+        System.out.println("Interest rate is: " + rate);
+        System.out.println("Your balance is: " + total);
+    }
+}
